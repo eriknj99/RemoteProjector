ad7a75242f71dd85ea8f3da4bc066c7fe0d9bf68
Erik Olsen <erik@eriknj99.com>
2022-01-22 12:52:52 -0500

    Finished client implementation including status printing

--- client/client.py
+++ client/client.py
@@ -1,16 +1,28 @@
 import os
 import shutil
 import socket
+import time
 from time import sleep
 import socket
-import log
 from termcolor import colored
 import json
+
+import bar
+import log
 
 SEPARATOR = "<SEP>"
 BUFFER_SIZE = 4096
 HOLD_TIME = 0.1 # Sleep time in seconds before sending a query
 
+# Take a duration in seconds and convert it to a readable format
+def formatTime(duration):
+    if(duration < 60):
+        return str(int(duration)) + "s"
+
+    if(duration < 3600):
+        return time.strftime('%M:%S', time.gmtime(int(duration)))
+
+    return time.strftime('%H:%M:%S', time.gmtime(int(duration)))   
 
 # A data class for keeping track of device progress
 class deviceStatus:
@@ -36,13 +48,20 @@ class deviceStatus:
 class NodeInfo:
 
     def to_string(self):
+        color = 'magenta'
+        gpu_color = 'blue'
+        if(self.device == 'CUDA'):
+            gpu_color = 'green'
+        if(self.device == 'OPENCL'):
+            gpu_color = 'red'
+
         out = ""
-        out += f"{colored('Name', 'magenta')}        : {self.name}\n"
-        out += f"{colored('Device', 'magenta')}      : {self.device}\n"
-        out += f"{colored('Blender EXE', 'magenta')} : {self.blender_exe}\n"
-        out += f"{colored('Blender Ver', 'magenta')} : {self.blender_ver}\n"
-        out += f"{colored('Host', 'magenta')}        : {self.host}\n"
-        out += f"{colored('Port', 'magenta')}        : {self.port}\n"
+        out += f"{colored('Name', color)}        : {self.name}\n"
+        out += f"{colored('Device', color)}      : {colored(self.device, gpu_color)}\n"
+        out += f"{colored('Blender EXE', color)} : {self.blender_exe}\n"
+        out += f"{colored('Blender Ver', color)} : {self.blender_ver}\n"
+        out += f"{colored('Host', color)}        : {self.host}\n"
+        out += f"{colored('Port', color)}        : {self.port}\n"
         return out
 
 
@@ -130,8 +149,9 @@ class NodeHandler:
                 s.connect((self.HOST, self.PORT))
                 s.sendall(f"render".encode())
                 data = s.recv(BUFFER_SIZE)
-                print(data.decode())
                 s.close()
+
+    
 
     def get_render_status(self):
         sleep(HOLD_TIME)
@@ -163,21 +183,51 @@ class NodeHandler:
     
     def begin_render(self, source_file, destination_file):
         self.current_dest = destination_file
-        log.debug("Starting Render")
         self.send_file(source_file, self.input_image)
         self.render()
+        self.render_start_time = time.time()
 
     def is_render_complete(self)->bool:
         return self.get_render_status().done
 
     def end_render(self):
         if(self.current_dest != None):
-            log.debug("Ending Render")
+            
+            self.total_time += time.time() - self.render_start_time
+            self.total_rendered += 1
+            
             self.recieve_file(self.output_image, self.current_dest)
 
     def check_connection(self)->bool:
         self.node = NodeInfo(self.get_info())
         return self.node.connected
+    
+    def to_string(self):
+        color = 'magenta'
+        self.node.host = self.HOST
+        out = self.node.to_string()
+        if(self.state == -1):
+            out += f"{colored('Status', color)}      : {colored('Ready', 'green')}\n"
+        elif(self.state == 0):
+            out += f"{colored('Status', color)}      : {colored('Rendering', 'blue')}\n"
+            out += f"{colored('Job', color)}         : {self.job_id}\n"
+            file_path = self.current_dest.split("/")
+            file_name = file_path[len(file_path) - 2:]
+            out += f"{colored('File', color)}        : {file_name[0]}/{file_name[1]}\n"
+            out += f"{colored('Time', color)}        : {formatTime(time.time() - self.render_start_time)}\n"
+            
+            out += f"{colored('Rendered', color)}    : {self.total_rendered} frames\n"
+            if(self.total_rendered != 0):
+                out += f"{colored('Avg time', color)}    : {formatTime(self.total_time / self.total_rendered)}\n"
+
+
+        elif(self.state == 1):
+            out += f"{colored('Status', color)}      : {colored('Idle', 'red')}\n"
+            out += f"{colored('Rendered', color)}    : {self.total_rendered} frames\n"
+            if(self.total_rendered != 0):
+                out += f"{colored('Avg time', color)}    : {formatTime(self.total_time / self.total_rendered)}\n"
+
+        return out
 
     def __init__(self, host, port, job_id, blend_file, input_image, output_image):
         self.HOST = host
@@ -187,6 +237,12 @@ class NodeHandler:
         self.input_image = input_image
         self.output_image = output_image
         self.current_dest = None
+        
+        self.total_rendered = 0
+        self.total_time = 0
+        self.render_start_time = -1
+
+        self.state = -1
         if(self.check_connection()):
             log.debug(f"Connected to {self.node.name}@{self.HOST}:{self.PORT}")
         else:
@@ -201,9 +257,16 @@ class NodeHandler:
         # Send the blender file to the node
         self.send_file(blend_file, blend_file)
 
-        print(self.node.to_string())
+def print_status(completed_frames, total_frames, nodes):
+    log.clear()
+    progress_bar = bar.drawGraph("Frames", "", completed_frames, 0, total_frames, color="blue", precision=0, numTicks = 3, length=-1)
+    print(progress_bar)
+    print('━' * 20)
+    for node in nodes:
+        print(node.to_string())
+        print('━' * 20)
 
-
+# Read a client config file as a json dict
 def read_config():
     out = {}
     f = open("config.json")
@@ -211,7 +274,6 @@ def read_config():
     for i in range(len(data)):
         out[data[i][0]] = data[i][1]
     return out 
-
  
 # Clear the given directory including sub dirs 
 def clear_dir(dir:str):
@@ -222,23 +284,31 @@ def clear_dir(dir:str):
         if(os.path.isdir(path)):
             shutil.rmtree(path)
 
-
-def init_rendered_dir(cache_dir, generated):
-    ren_dir = os.path.join(cache_dir, "rendered")
-    clear_dir(ren_dir)
-    for dir in generated.keys():
-        new_dir = os.path.join(ren_dir, dir)
+# If the dir does not exist, create it, else delete its contents 
+def init_output_dir(output_dir):
+    if(os.path.isdir(output_dir)):
+        clear_dir(output_dir)
+    else:
         os.mkdir(new_dir)
 
-def get_generated(cache_dir):
+# Get a dict of input image -> output image from 2 ordered arrays of input_dirs,output_dirs
+def build_render_queue(input_dirs, output_dirs):
     out = {}
-    gen_dir = os.path.join(cache_dir, "generated")
-    subdirs = [f for f in os.listdir(gen_dir) if os.path.isdir(os.path.join(gen_dir, f))]
-    for sd in subdirs:
-        img_dir = os.path.join(gen_dir, sd)
-        out[sd] = [f for f in os.listdir(img_dir) if os.path.isfile(os.path.join(img_dir, f))]
+    for i in range(len(input_dirs)):
+        source_files = ls_abs(input_dirs[i])
+        for file in source_files:
+            out[file] = os.path.join(output_dirs[i], os.path.basename(file))
     return out
 
+# Get an array of absolute paths of files in a dir
+def ls_abs(input_dir):
+    file_paths = []
+    for folder, subs, files in os.walk(input_dir):
+        for filename in files:
+            file_paths.append(os.path.abspath(os.path.join(folder, filename)))
+    return file_paths
+
+# Find the blender file in a directory
 def get_blend(cache_dir):
     files = [f for f in os.listdir(cache_dir) if os.path.isfile(os.path.join(cache_dir, f))]
     for f in files:
@@ -246,68 +316,71 @@ def get_blend(cache_dir):
             return os.path.join(cache_dir, f)
     return None
 
+# Get the cache name from the abs path of the cache 
 def parse_cache_name(cache_dir)->str:
     return os.path.basename(cache_dir)
 
-def build_render_queue(cache_dir):
-    out = {}
-    gen_dir = os.path.join(cache_dir, "generated")
-    subdirs = [f for f in os.listdir(gen_dir) if os.path.isdir(os.path.join(gen_dir, f))]
-    for sd in subdirs:
-        img_dir = os.path.join(gen_dir, sd)
-        imgs = os.listdir(img_dir)
-        for img in imgs:
-            gen_img_path = os.path.join(img_dir, img)
-            ren_img_path = gen_img_path.replace("generated", "rendered")
-            out[gen_img_path] = ren_img_path
-    return out
 
-def project(cache_dir, input_image="input.png", output_image="0000.png"):
-    log.debug("Scanning cache dir...")
-    gen_structure = get_generated(cache_dir)
-     
-    init_rendered_dir(cache_dir, gen_structure)
-    blend_file = get_blend(cache_dir)
-    if(blend_file == None):
-        log.error("No blender files found")
+# job_id         : Does not matter, should be cache name
+# input_folders  : A list of abs paths to directories containing images
+# output_folders : A list of abs paths in the same order as input_folders where the output will be stored
+# input_image    : The name of the image that the blend file is expecting
+# output_image   : The name of the file that the blend file will output
+def project(job_id, blend_file, input_folders, output_folders, input_image="input.png", output_image="0000.png"):
     
-    render_queue = build_render_queue(cache_dir)
+    # Init the given output directories
+    for output_dir in output_folders:
+        init_output_dir(output_dir)
+
+    # Build the render queue from the input/output folders
+    render_queue = build_render_queue(input_folders, output_folders)
     
-    log.debug("Initializing nodes...")
+    # Initialize the nodes from the config file
     node_configs = read_config()
     nodes = []
     for nc in node_configs.keys():
-        nodes.append(NodeHandler(nc, node_configs[nc], parse_cache_name(cache_dir), blend_file, input_image, output_image))
+        nodes.append(NodeHandler(nc, node_configs[nc], job_id, blend_file, input_image, output_image))
+   
+    # Init vars to keep track of render progress
+    total_frames = len(render_queue)
+    completed_frames = 0
     
-    log.debug("Starting render process...")
-
-    for node in nodes:
-        input = list(render_queue.keys())[0]
-        output = render_queue.pop(input)
-        node.begin_render(input, output)
-    
-    while(len(render_queue) != 0):
+    # Dispatch all render jobs
+    done = False
+    while(not done):
+        print_status(completed_frames, total_frames, nodes)
+        done = True
         for node in nodes:
-            if(node.is_render_complete()):
-                node.end_render()
+            # -1 : Node has not been given a render job yet
+            if(node.state == -1):
+                done = False
                 input = list(render_queue.keys())[0]
                 output = render_queue.pop(input)
                 node.begin_render(input, output)
-        sleep(1)
+                node.state = 0
+                
 
+            # 0 : Node is currently processing jobs
+            elif(node.state == 0):
+                done = False
+                if(node.is_render_complete()):
+                    node.end_render()
+                    completed_frames+=1
+                    # When there are no more jobs set state to 1
+                    if(len(render_queue) == 0):
+                        node.state = 1
+                    else:
+                        input = list(render_queue.keys())[0]
+                        output = render_queue.pop(input)
+                        node.begin_render(input, output)
+
+        sleep(1)
     
 
+blend_file   = "/home/erik/Projects/Neon2/Neon/Cache/remote_test/test.blend"
+input_files  = ["/home/erik/Projects/Neon2/Neon/Cache/remote_test/generated/main_mask", "/home/erik/Projects/Neon2/Neon/Cache/remote_test/generated/hlines", "/home/erik/Projects/Neon2/Neon/Cache/remote_test/generated/vlines"]
+output_files = ["/home/erik/Projects/Neon2/Neon/Cache/remote_test/rendered/main_mask", "/home/erik/Projects/Neon2/Neon/Cache/remote_test/rendered/hlines", "/home/erik/Projects/Neon2/Neon/Cache/remote_test/rendered/vlines"]
 
+project("remote_test", blend_file, input_files, output_files)
 
-cache_dir = "/home/erik/Projects/Neon2/Neon/Cache/remote_test"
-project(cache_dir)
-
-
-
-#nh = NodeHandler("127.0.0.1", 5002, "TEST_JOB", "test.blend", "input.png", "0000.png")
-
-#nh.begin_render("AAA.png")
-#while(nh.is_render_complete() == False):
-#    sleep(1)
-#nh.end_render("output.png")
 
